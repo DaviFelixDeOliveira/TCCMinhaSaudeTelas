4760e3967b3515da958858d3d321433ded04ba31
DaviFelixDeOliveira <davi.oliveira153@etec.sp.gov.br>
2025-08-25 11:18:23 -0300

    Instalando o flutter

---
+++ telas/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/LogoMinhaSaude.png'),
+      ),
+    ),
+  ));
+}
